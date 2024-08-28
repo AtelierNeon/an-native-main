63e67acbf8a1582f5f19be53f10119a6775ae917
Kene Lin <kenelin@gmail.com>
2024-08-28 15:55:35 +0800

    Add build script (#1)
    * Add build script
    
    * Add build script, part 2

---
+++ script/build-linux-preset.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+##
+## Global config
+##
+DIRNAME_CLI=/usr/bin/dirname
+PWD_CLI=/bin/pwd
+UNAME_CLI=/bin/uname
+SYSTEM_PLATFORM=$($UNAME_CLI)
+
+echo "[$SYSTEM_PLATFORM] Applying preset options ..."
+MY_PROJECT_EXPAT_WITHOUT_APPS=ON
+MY_PROJECT_EXPAT_WITHOUT_TEST_APPS=ON
+echo "[$SYSTEM_PLATFORM] Applying default options ... DONE"
+
+SCRIPT_DIR=$(cd -- "$(${DIRNAME_CLI} "${BASH_SOURCE[0]:-${(%):-%x}}")" &> /dev/null && ${PWD_CLI})
+source $SCRIPT_DIR/build-linux.bash
---
+++ script/build-windows-preset.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+setlocal
+echo [Windows] Applying preset options ...
+set MY_PROJECT_EXPAT_WITHOUT_APPS=ON
+set MY_PROJECT_EXPAT_WITHOUT_INSTALL_ALL=ON
+set MY_PROJECT_EXPAT_WITHOUT_TEST_APPS=ON
+echo [Windows] Applying default options ... DONE
+call %~dp0\build-windows.cmd
+if "%ERRORLEVEL%" neq "0" (
+    exit /b !ERRORLEVEL!
+)
+endlocal
