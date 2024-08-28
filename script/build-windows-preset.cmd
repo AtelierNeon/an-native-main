b782cc917b116b4008618e91a795c38a48f09fdc
Kene Lin <kenelin@gmail.com>
2024-08-28 14:21:29 +0800

    Add build script

---
+++ script/build-windows-preset.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+setlocal
+echo [Windows] Applying preset options ...
+set MY_PROJECT_EXPAT_WITHOUT_APPS=ON
+set MY_PROJECT_EXPAT_WITHOUT_TEST_APPS=ON
+echo [Windows] Applying default options ... DONE
+call %~dp0\build-windows.cmd
+if "%ERRORLEVEL%" neq "0" (
+    exit /b !ERRORLEVEL!
+)
+endlocal
