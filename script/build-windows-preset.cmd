8f69f91aa1cfe7ab358bd9e785123f57dfab518e
Kene Lin <kenelin@gmail.com>
2024-08-28 18:30:09 +0800

    Import SQLite

--- script/build-windows-preset.cmd
+++ script/build-windows-preset.cmd
@@ -5,6 +5,8 @@ echo [Windows] Applying preset options ...
 set MY_PROJECT_EXPAT_WITHOUT_APPS=ON
 set MY_PROJECT_EXPAT_WITHOUT_INSTALL_ALL=ON
 set MY_PROJECT_EXPAT_WITHOUT_TEST_APPS=ON
+set MY_PROJECT_SQLITE_WITHOUT_APPS=ON
+set MY_PROJECT_SQLITE_WITHOUT_INSTALL_ALL=ON
 echo [Windows] Applying default options ... DONE
 call %~dp0\build-windows.cmd
 if "%ERRORLEVEL%" neq "0" (
