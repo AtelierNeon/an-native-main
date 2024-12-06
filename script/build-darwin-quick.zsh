928d119841ba69ff65d1fbf4440037f84b0df3af
Kene Lin <kenelin@gmail.com>
2024-12-06 23:34:17 +0800

    Update build script to support compiler cache & prechecks

--- script/build-darwin-quick.zsh
+++ script/build-darwin-quick.zsh
@@ -10,6 +10,7 @@ SYSTEM_PLATFORM=$($UNAME_CLI)
 
 echo "[$SYSTEM_PLATFORM] Running quick (re)build ..."
 MY_PROJECT_SHOULD_DISABLE_CLEAN_BUILD=ON
+MY_PROJECT_WITH_COMPILER_CACHE=ON
 
 SCRIPT_DIR=$(cd -- "$(${DIRNAME_CLI} "${BASH_SOURCE[0]:-${(%):-%x}}")" &> /dev/null && ${PWD_CLI})
 source $SCRIPT_DIR/build-darwin-preset.zsh
